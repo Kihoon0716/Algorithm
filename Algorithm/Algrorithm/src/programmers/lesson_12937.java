edab8bdaddc7d3f0ff6540a0ab7709ff4dd7a766
InhyeJeong <okoooc@hanmail.net>
2019-03-04 12:17:46 +0900

    프로그래머스 12937 홀짝 출력

---
+++ Algorithm/Algrorithm/src/programmers/lesson_12937.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        //    짝수일 경우
+        if(num  % 2 == 0) {
+            return "Even";
+        } else return "Odd";
+    }
+  }
