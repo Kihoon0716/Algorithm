02f2a8695dbe11522df270c3f92b3746a66799cf
InhyeJeong <okoooc@hanmail.net>
2019-03-04 11:12:01 +0900

    프로그래머스 12918 문자열 다루기 기본

---
+++ Algorithm/SwExpertAcademy/src/programmers/lesson_12918.java
@@ -0,0 +1,25 @@
+//  문자열 다루기 기본
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        //    문자열의 길이 확인
+        if( s.length() == 4 || s.length() == 6) {
+          //    숫자이면 t 리턴
+            if(isStringInt(s)) {
+                return true;
+          //    문자포함하면 f 리턴
+            } else {
+                return false;
+            }
+        } else return false;
+    }
+      
+       public static boolean isStringInt(String s) {
+          try {
+              Integer.parseInt(s);
+              return true;
+          } catch (NumberFormatException e) {
+              return false;
+          }
+    }
+  }
