a6158c3b0c92021d39b3735222475fbd0e6aaa73
InhyeJeong <okoooc@hanmail.net>
2019-03-05 10:54:25 +0900

    프로그래머스 12944 평균값

---
+++ Algorithm/Algrorithm/src/programmers/lesson_12933.js
@@ -0,0 +1,14 @@
+//  정수 내림차순 배치(정렬)
+function solution(n) {
+    let answer = 0;
+    const arr = [];
+    arr.push(n);
+
+    console.log(arr);
+    
+    // arr.sort(function (a, b) {    //  내림차순
+    //     return b - a;
+    // })
+    // answer = arr;
+    return answer;
+}
